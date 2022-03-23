bc6944721f6c29e2027922cce2c4329aae338276
toyuu16 <ssta0ut0@gmail.com>
2022-03-23 23:38:20 +0900

    ファイル追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App';
+import App from './pages/app';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
 import { Route, BrowserRouter } from "react-router-dom";
 import Profile from './pages/profile';
+import Login from './pages/login';
+import SignUp from './pages/signUp';
+import Home from './pages/home';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <div>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/app" component={App} />
         <Route exact path="/profile" component={Profile} />
       </div>
     </BrowserRouter>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -27,45 +27,47 @@ const App = () => {
 
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible>
-        <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            <Link to="/profile">Profile</Link>
+    <>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              <Link to="/profile">Profile</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
           </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User" >
+              <Menu.Item key="3" id='user' onClick={username} >Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />}>
+              Files
           </Menu.Item>
-          <SubMenu key="sub1" icon={<UserOutlined />} title="User" >
-            <Menu.Item key="3" id='user' onClick={username} >Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9" icon={<FileOutlined />}>
-            Files
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            Bill is a cat.
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
             </div>
-          <Icon></Icon>
-        </Content >
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            <Icon></Icon>
+          </Content >
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout >
       </Layout >
-    </Layout >
+    </>
   );
 }
 
